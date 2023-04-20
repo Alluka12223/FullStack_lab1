378ca238e4973f3f852d9a657db76186dfbf4820
Jasmin Akter Chowdhury <jasmine.chy12223@gmail.com>
2023-04-21 00:01:34 +0200

    all routes added

--- public/client.js
+++ public/client.js
@@ -17,6 +17,43 @@ async function getAllAlbums() {
   });
 }
 
+async function albumDetails(album, row) {
+  const actionsCell = row.insertCell();
+  const detailsButton = document.createElement("button");
+  detailsButton.innerText = "Details";
+  detailsButton.addEventListener("click", async () => {
+    const detailsTable = document.getElementById("details-table");
+    if (detailsTable.getAttribute("hidden")) {
+      detailsTable.removeAttribute("hidden");
+      document.getElementById("table-heading-text").removeAttribute('hidden')
+    }
+    detailsTable.querySelector("#details-id").textContent = album._id;
+    detailsTable.querySelector("#details-title").textContent = album.title;
+    detailsTable.querySelector("#details-artist").textContent = album.artist;
+    detailsTable.querySelector("#details-year").textContent = album.year;
+  });
+  actionsCell.appendChild(detailsButton);
+}
+
+const deleteButton = document.getElementById("deleteButton");
+deleteButton.addEventListener("click", async (event) => {
+  // event.preventDefault();
+  const target = event.target;
+  if (confirm("Are you sure you want to delete this album?")) {
+    const row = target.parentNode.parentNode
+    await getIdFrom_id(row);
+    try {
+      await fetch(`http://localhost:3000/api/albums/${idFrom_id}`, {
+        method: "DELETE",
+      });
+      row.remove();
+    } catch (err) {
+      console.error(err.message);
+    }
+  }
+  await getAllAlbums();
+});
+
 async function createAlbumCollapse() {
   let coll = document.getElementsByClassName("createButton");
   for (let i = 0; i < coll.length; i++) {
@@ -52,7 +89,7 @@ async function searchByTitle() {
     } else if (searchedAlbum.status === 404){
       res+="<p>No such Album can be found in the database!</p>"
     }
-    albumArea.innerHTML=res
+    albumArea.innerHTML = res
     searchedAlbum = await searchedAlbum.json();
     if (searchedAlbum == undefined) {
       res += "<p>Album not found</p>";
@@ -86,4 +123,103 @@ searchButton.addEventListener("click", async (event) => {
 window.addEventListener("load", () => {
   getAllAlbums();
   createAlbumCollapse()
+});
+
+const updateButton = document.getElementById("updateButton");
+let idFrom_id;
+updateButton.addEventListener("click", async (event) => {
+  const target = event.target;
+
+  if (target.classList.contains("btn-update")) {
+    const row = target.parentNode.parentNode;
+    const cells = row.querySelectorAll("td:not(:last-child)");
+
+    cells.forEach((cell) => {
+      const text = cell.innerHTML.trim();
+      cell.innerHTML = `<input type="text" value="${text}">`;
+    });
+
+    const updateBtn = row.querySelector(".btn-update");
+    updateBtn.classList.remove("btn-update");
+    updateBtn.classList.add("btn-save");
+    updateBtn.textContent = "Save";
+    return;
+  }
+  if (target.classList.contains("btn-save")) {
+    const row = target.parentNode.parentNode;
+    const cells = row.querySelectorAll("td:not(:last-child)");
+
+    cells.forEach((cell) => {
+      const text = cell.querySelector("input").value;
+      cell.innerHTML = "";
+      cell.textContent = text;
+    });
+
+    const saveBtn = row.querySelector(".btn-save");
+    saveBtn.classList.remove("btn-save");
+    saveBtn.classList.add("btn-update");
+    saveBtn.textContent = "Update";
+
+    const title = cells[1].textContent;
+    const artist = cells[2].textContent;
+    const year = cells[3].textContent;
+    await getIdFrom_id(row)
+    try {
+
+      const upToDate = await fetch(
+        `http://localhost:3000/api/albums/${idFrom_id}`,
+        {
+          method: "PUT",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({ title, artist, year }),
+        }
+      );
+      await upToDate.json();
+    } catch (error) {
+      console.log(error);
+    }
+    await getAllAlbums();
+  }
+});
+
+async function getIdFrom_id(row) {
+  const id = row.querySelector("#details-id").textContent;
+  let albums = await fetch("http://localhost:3000/api/albums", {
+    method: "GET",
+    headers: { "content-type": "application/json" },
+  });
+  albums = await albums.json()
+  albums.forEach((album) => {
+    if (album._id === id) {
+      idFrom_id = album.id;
+    }
+  });
+}
+
+const postAlbum = document.getElementById("create-album-form");
+postAlbum.addEventListener("submit", async (event) => {
+  event.preventDefault();
+  const title = document.getElementById("title").value.trim();
+  const artist = document.getElementById("artist").value.trim();
+  const year = document.getElementById("year").value.trim();
+  try {
+    const response = await fetch("http://localhost:3000/api/albums", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ title, artist, year }),
+    });
+    if (!response.ok) {
+      throw new Error("Failed to create album");
+    }
+    await getAllAlbums();
+    document.getElementById("title").value = "";
+    document.getElementById("artist").value = "";
+    document.getElementById("year").value = "";
+  } catch (err) {
+    console.error(err.message);
+  }
 });
--- routes/api.js
+++ routes/api.js
@@ -58,6 +58,49 @@ router.get("/:title", async (req, res) => {
   }
 });
 
+router.put("/:id", async (req, res) => {
+  const id = req.params.id;
+  if (albumExistsById(id, res)) {
+    const album = await albumExistsById(id, res)
+    try {
+      album.title = req.body.title;
+      album.artist = req.body.artist;
+      album.year = req.body.year;
+      const result = await album.save();
+      res.json(result);
+    } catch (err) {
+      res.status(500).send(err.message);
+    }
+  }
+})
 
+router.delete(("/:id"), async (req, res) => {
+  const id = req.params.id;
+  if (albumExistsById(id, res)) {
+    const album = await albumExistsById(id, res)
+    try {
+      // album = res.album;
+      console.log('ress', album, "sda",res);
+      await album.deleteOne();
+      res.json({ message: "Successfully Deleted!" });
+    } catch (err) {
+      res.status(500).send(err.message);
+    }
+  }
+});
+
+async function albumExistsById(id, res) {
+  let album = await Album.find({ id: id }).exec();
+  if (album.length === 0) {
+    return res.status(404).send(`No such album with id ${id} in the database!`);
+  }
+  return album[0];
+}
+
+const generateID = async () => {
+  const lastDoc = await Album.find({}).sort({ _id: -1 }).limit(1);
+  const ID = lastDoc[0].id + 1;
+  return ID;
+}
 
 module.exports = router;
