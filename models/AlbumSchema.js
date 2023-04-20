235cdbfea36130ac313c053128e40d8152688f45
Jasmin Akter Chowdhury <jasmine.chy12223@gmail.com>
2023-04-20 22:24:36 +0200

    mongoose data created

--- models/AlbumSchema.js
+++ models/AlbumSchema.js
@@ -1,16 +1,20 @@
 const mongoose = require("mongoose");
 
 const album = new mongoose.Schema({
+  id:
+  {
+    type: Number,
+    required: true,
+    unique: true
+  },
   title:
   {
     type: String,
     required: true,
-    // collation: { locale: "en", strength: 2 },
   },
   artist: {
     type: String,
     required: true,
-    // collation: { locale: "en", strength: 2 },
   },
   year: {
     type: Number,
