616a6d8f36b2eb8fea0a5a55b243ccf8a4870c20
Jasmin Akter Chowdhury <jasmine.chy12223@gmail.com>
2023-04-28 14:36:55 +0200

    remove optional port

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const routes = require("./routes/api.js");
 require("dotenv").config();
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT;
 
 app.use(cors({ origin: "*", }));
 app.use(bodyParser.urlencoded({extended: true}));
